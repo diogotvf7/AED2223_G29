32dc4b939356407da67234d950419b8df87d0ad1
diogotvf7 <up202108752@g.uporto.pt>
2022-12-31 05:17:11 +0000

    Added statistics, updated CMakeLists

---
+++ Statistics.hpp
@@ -0,0 +1,59 @@
+//
+// Created by diogotvf7 on 30-12-2022.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <map>
+
+#include "DataManager.h"
+#include "Coordinate.h"
+
+using namespace std;
+
+class Statistics {
+    DataManager dm;
+
+public:
+
+    Statistics() {
+        dm = DataManager();
+        dm.readAirlines();
+        dm.readAirports();
+    }
+
+    void airportsByCountry() {
+
+        cout << setw(58) << right << "Airports by country:\n";
+        cout << setw(58) << right << "____________________\n";
+
+        airportsUM airports = dm.getAirports();
+        map<string,int> freqTable;
+        for (auto &[code, airport] : airports)
+            freqTable[airport->getCountry()]++;
+        for (auto &[country, freq] : freqTable)
+            cout << "   :" << setw(40) << right << country << "  --  " << freq << " airports" << '\n';
+
+        cout << '\n';
+    }
+
+    void airlinesByCountry() {
+
+        cout << setw(58) << right << "Airlines by country:\n";
+        cout << setw(58) << right << "____________________\n";
+
+        airlinesUM airlines = dm.getAirlines();
+        map<string,int> freqTable;
+        for (auto &[code, airline] : airlines)
+            freqTable[airline->getCountry()]++;
+        for (auto &[country, freq] : freqTable)
+            cout << "   :" << setw(40) << right << country << "  --  " << freq << " airlines" << '\n';
+
+        cout << '\n';
+    }
+
+    void runStatistics() {
+        airportsByCountry();
+        airlinesByCountry();
+    };
+};
--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 
 #include "DataManager.h"
 #include "Test.hpp"
+#include "Statistics.hpp"
 
 using namespace std;
 
 int main() {
     Test t; t.runTests();
+    // Statistics s; s.runStatistics();
     DataManager dm;
     dm.readAirports();
     dm.readAirlines();
